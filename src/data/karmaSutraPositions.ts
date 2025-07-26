5f6b0acd95f95676d90a63e11b09fdb759cad2fb
alastrass <154005517+alastrass@users.noreply.github.com>
2025-07-27 01:13:15 +0200

    Updated karmaSutraPositions.ts

--- src/data/karmaSutraPositions.ts
+++ src/data/karmaSutraPositions.ts
@@ -6,8 +6,8 @@ export const karmaSutraPositions: KarmaSutraPosition[] = [
     name: "Le Lotus",
     description: "Position assise face à face, jambes entrelacées",
     difficulty: "facile",
-    illustration: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.feter-recevoir.com%2Fjeu-54-cartes-a-jouer-position-kamasutra-comic%2F&psig=AOvVaw2ou2yoz3r1c7tlF9i-n0Ma&ust=1753657525685000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOD6wOLR244DFQAAAAAdAAAAABAE",
-    imageUrl: "", // Vous pouvez ajouter le lien ici
+    illustration: "👩‍❤️‍👨",
+    imageUrl: "https://mak3r.fun/wp-content/uploads/2025/07/lotus.jpeg", // Vous pouvez ajouter le lien ici
     benefits: ["Intimité maximale", "Contact visuel", "Contrôle partagé"]
   },
   {
